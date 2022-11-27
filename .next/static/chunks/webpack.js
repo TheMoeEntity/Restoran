5c8779fb2d0176e6a4fa40689fea3bf1760d44c6
Moses Nwigberi <mosesnwigberi@gmail.com>
2022-11-27 16:27:40 +0100

    Added transitions

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "95ffb11e8586f115"; }
+/******/ 		__webpack_require__.h = function() { return "7c9b1a95d3b3dd6e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
