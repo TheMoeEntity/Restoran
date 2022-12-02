a604e185f1c210628fe83be8508043b5c6262613
Moses Nwigberi <mosesnwigberi@gmail.com>
2022-12-02 11:37:43 +0100

    fixed oveflow problem

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d22a239b33469c57"; }
+/******/ 		__webpack_require__.h = function() { return "b2d71f5dbcdf8e0f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
