cf566fbc584814b48f848a2c1687ca5603e53d5e
Moses Nwigberi <mosesnwigberi@gmail.com>
2023-06-30 20:35:56 +0100

    .

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2619d141aecc3985"; }
+/******/ 		__webpack_require__.h = function() { return "cd549f0851954210"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Gallery/Gallery.jsx
+++ components/Gallery/Gallery.jsx
@@ -1,19 +1,19 @@
-import styles from './Gallery.module.css'
+import styles from "./Gallery.module.css";
 
 const Gallery = () => {
   return (
     <div className={styles.gallery}>
-        <div className={styles.caption}>
-            <h3>Gallery</h3>
-            <h2>Our Foods <span>Gallery</span></h2>
-            <p>
-            Lorem ipsum dolor sit amet, consectetur <br /> adipisicing elit. Aperiam iusto nesciunt nisi, placeat inventore similique.
-            </p>
-            <input type="text" /><input type="text" /><input type="text" />
-
-        </div>
+      <div className={styles.caption}>
+        <h3>Gallery</h3>
+        <h2>
+          Our Foods <span>Gallery</span>
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur <br /> adipisicing elit.
+          Aperiam iusto nesciunt nisi, placeat inventore similique.
+        </p>
+      </div>
     </div>
-  )
-}
-
-export default Gallery
+  );
+};
+export default Gallery;
